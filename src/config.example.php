623ec1d615fcaff282c3116341359a49623b29b5
YesterDavid135 <minderdadavid135@gmail.com>
2022-09-20 15:03:17 +0200

    fixed example config

--- src/config.example.php
+++ src/config.example.php
@@ -4,16 +4,16 @@
  * Please copy this file to config.php and adjust the variables on line 7-10
 */
 
-define('DB_SERVER', 'hostname');
-define('DB_USERNAME', 'username');
-define('DB_PASSWORD', 'password');
-define('DB_NAME', 'dbname');
+define('DB_SERVER', '<hostname>');
+define('DB_USERNAME', '<username>');
+define('DB_PASSWORD', '<password>');
+define('DB_NAME', '<dbname>');
 
 // connect to database
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+if ($link->connect_error) {
+    die("Connection failed: " . $link->connect_error);
 }
 ?>
