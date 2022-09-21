399f5fb64af84a3d482b461b0c7e05a1538b3251
yesterdavid135 <david.minder@sunrise.ch>
2022-09-21 15:47:42 +0200

    finished dynamic pricing

--- src/order.php
+++ src/order.php
@@ -76,7 +76,7 @@
                         if ($row["discount"])
                             print ('<div>
                                         <p class="link-dark fw-bold text-decoration-line-through">' . $row["price"].  ' CHF </p>
-                                        <h4 class="link-danger fw-bold ">' .  ($row["price"] / 100) * (100 - $row['discount']) .  ' CHF <span class="badge bg-danger">10%</span></h4>
+                                        <h4 class="link-danger fw-bold ">' .  ($row["price"] / 100) * (100 - $row['discount']) .  ' CHF <span class="badge bg-danger">' . $row['discount'].'%</span></h4>
                                     </div>
                                     <button type="button" class="btn btn-sm btn-outline-primary">Add to cart</button>
                                 </div>');
