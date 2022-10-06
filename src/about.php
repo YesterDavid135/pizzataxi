0c89bb6d40cefd3900bef88db338c35be525cd15
cashlessxd <mnoel8393@gmail.com>
2022-10-06 23:34:33 +0200

    fixed footer to bottom / changed colors / added better icons for index site

--- src/about.php
+++ src/about.php
@@ -19,7 +19,7 @@ session_start();
             src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6177030326507154"
             crossorigin="anonymous"></script>
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
 <!-- Responsive navbar-->
 <?php
 include('navbar.php');
@@ -39,12 +39,12 @@ include('navbar.php');
     <div class="p-4 p-lg-5 rounded-3 text-center">
         <div class="m-4 m-lg-5">
             <p class="fs-4">This is a school project. Please do NOT give us your credit card details. (If you really want to, you can still do it.)</p>
-            <a class="btn btn-primary btn-lg" href="https://www.google.com/search?q=Order+Pizza&rlz=1C1GCEU_enCH927CH927&oq=order+pizza&aqs=chrome..69i57j0i512j0i457i512j0i512j0i22i30l2j69i60l2.5303j0j7&sourceid=chrome&ie=UTF-8">Order a real pizza now!</a>
+            <a class="btn btn-warning btn-lg" href="https://www.google.com/search?q=Order+Pizza&rlz=1C1GCEU_enCH927CH927&oq=order+pizza&aqs=chrome..69i57j0i512j0i457i512j0i512j0i22i30l2j69i60l2.5303j0j7&sourceid=chrome&ie=UTF-8">Order a real pizza now!</a>
         </div>
     </div>
 </div>
 <!-- Footer-->
-<footer class="py-5 bg-dark">
+<footer class="py-5 bg-dark mt-auto">
     <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Pizzataxi 2022</p></div>
 </footer>
 <!-- Bootstrap core JS-->
