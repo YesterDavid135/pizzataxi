5c51c6ae26be7ac34f044ef81cf708910408234c
YesterDavid135 <minderdadavid135@gmail.com>
2022-10-07 23:12:17 +0200

    Design fix on 404 page

--- src/404.php
+++ src/404.php
@@ -10,40 +10,19 @@ session_start();
     <meta name="author" content="" />
     <title>Pizzataxi</title>
     <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-    <link href="css/custom.css" rel="stylesheet" />
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico"/>
+    <link href="css/custom.css" rel="stylesheet"/>
     <!-- Bootstrap icons-->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet"/>
     <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="css/styles.css" rel="stylesheet"/>
 
 </head>
-<body>
+<body class="d-flex flex-column min-vh-100">
 <!-- Responsive navbar-->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container px-lg-5">
-        <a class="navbar-brand" href="#!">Pizzataxi</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
-                <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                <li class="nav-item"><a class="nav-link" href="menu.php">Order</a></li>
-                <li class="nav-item"><a class="nav-link" href="#!">Contact</a></li>
-                <li class="nav-item"><a class="nav-link" href="about.php">About Us</a></li>
-                <?php
-                if (!isset($_SESSION['username'])) {
-                    print('
-                        <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li>
-                        <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
-                    ');
-                } else {
-                    print('<li class="nav-item"><a class="nav-link" href="logout.php">Logout</a></li>');
-                }
-                ?>
-            </ul>
-        </div>
-    </div>
-</nav>
+<?php
+include('navbar.php');
+?>
 <!-- Header-->
 <header class="py-5">
     <div class="container px-lg-5">
@@ -62,7 +41,7 @@ session_start();
 </header>
 
 <!-- Footer-->
-<footer class="py-5 bg-dark">
+<footer class="py-5 bg-dark mt-0">
     <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Pizzataxi 2022</p></div>
 </footer>
 <!-- Bootstrap core JS-->
