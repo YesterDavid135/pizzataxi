33e69650388f63cfb1074ce7e430532a041c7632
cashlessxd <mnoel8393@gmail.com>
2023-12-11 13:32:47 +0100

    added registration design

--- src/totp-gen.php
+++ src/totp-gen.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <?php
 require __DIR__ . '/../vendor/autoload.php';
 session_start();
@@ -70,7 +71,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
         $_SESSION['loggedin'] = true;
         header('Location: index.php');
     } else {
-        echo "Wrong code your poor ass guy";
+        $error .= "Oops, wrong code :(<br>";
     }
 } else {
     $secret = $g->createSecret();
@@ -98,13 +99,65 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 }
 
 $qrCodeUrl = $g->getQRCodeGoogleUrl($username, $secret, "PizzaTaxi");
-echo "QR-Code URL: " . $qrCodeUrl . "<br>";
-
-echo '<img src="' . htmlspecialchars($qrCodeUrl) . '" alt="TOTP QR Code">';
-
 ?>
-<form action="totp-gen.php" method="POST">
-    <label>Verify One Time Password</label>
-    <input name="totp">
-    <button type="submit"></button>
-</form>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Pizzataxi</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+    <!-- Bootstrap icons-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet">
+    <script async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6177030326507154"
+            crossorigin="anonymous"></script>
+</head>
+<body class="d-flex flex-column min-vh-100">
+    <!-- Responsive navbar-->
+    <?php
+    include('navbar.php');
+    ?>
+    <header class="pt-5">
+        <div class="container px-lg-5">
+            <div class="p-4 p-lg-5 rounded-3 text-center">
+                <div class="m-2 m-lg-0">
+                    <h1 class="display-5 fw-bold">2 Factor Authentication</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container px-lg-5">
+        <div class="p-4 p-lg-5 rounded-3">
+            <div class="m-lg-5 pb-4">
+                <?php
+                if (!empty($error)) {
+                    echo "<div class=\"alert alert-danger\" role=\"alert\">" . $error . "</div>";
+                }
+                ?>
+                <div class="text-center mb-5">
+                    <img src="<?=$qrCodeUrl?>" alt="qrcode">
+                </div>
+                <form action="totp-gen.php" method="POST">
+                    <div class="mb-3">
+                        <label for="totp" class="form-label">Verify One Time Password</label>
+                        <input name="totp" class="form-control">
+                    </div>
+                    <button type="submit" class="btn btn-dark">Verify</button>
+                </form>
+            </div>
+        </div>
+    </div>
+<!-- Footer-->
+<footer class="py-5 bg-dark mt-auto">
+    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Pizzataxi 2022</p></div>
+</footer>
+<!-- Bootstrap core JS-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<!-- Core theme JS-->
+<script src="js/scripts.js"></script>
+</body>
